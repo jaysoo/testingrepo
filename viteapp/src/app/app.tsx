b09c0cb56bb443e29248812ce042ed078daf5a94
Jack Hsu <jack.hsu@gmail.com>
2024-06-26 13:21:51 -0400

    update

--- viteapp/src/app/app.tsx
+++ viteapp/src/app/app.tsx
@@ -3,6 +3,9 @@ import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
 
+import {jslib} from '@testing/jslib';
+console.log(jslib());
+
 export function App() {
   return (
     <div>
